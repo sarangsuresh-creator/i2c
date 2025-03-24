0ecbf846337b0a7744976a0980f660359fe5e2dc
SARANG SURESH <sureshsarang44@gmail.com>
2025-03-24 09:33:29 +0530

    Update ST.c

--- I2C in pic18f4580/MR-ST/ST.c
+++ I2C in pic18f4580/MR-ST/ST.c
@@ -1,1 +1,61 @@
+/*
+ * File:   ST.c
+ * Author: LENOVO
+ *
+ * Created on 19 January, 2025, 8:48 PM
+ */
 
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+void data(const char d);
+void string(const char *s);
+int i;
+void main(void) {
+    SSPADD = 0Xd0;
+    SSPSTAT = 0X80;
+    SSPCON1 = 0X36;
+    SSPCON2 = 0x01;
+    TRISC = 0xff;
+    while(1){
+         if(SSPSTAT & 0X08){
+        SSPOV = 0;
+        //data =SSPBUF;
+    }
+      
+        if(CKP==0){
+            //string("sarang");
+            //CKP = 0;
+            if(SSPSTAT & 0X04){
+                char dummy = SSPBUF;
+                SSPBUF = 40 + i++;
+                while(!BF);
+                
+                
+                CKP =1;
+                SSPIF = 0;
+                
+                
+                
+               // __delay_ms(5);
+            }
+        }
+    }
+
+    return;
+}
+// string(const char *s){
+//    while(*s){
+//        data(*s++);
+//    }
+//}
+//void data(const char d){
+//if(SSPSTAT & 0X04){
+//                char dummy = SSPBUF;
+//                SSPBUF = 's';
+//                while(!BF);
+//                CKP =1;
+//                
+//                SSPIF = 0;
+//            }
+//}
